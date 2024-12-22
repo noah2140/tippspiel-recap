5f3ba3ecd48bde4e390b88331f6055dc38fe0c11
Noah Ickenroth <noahickenroth@uni-koblenz.de>
2024-12-22 23:35:39 +0100

    Tried change to Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import Layout from "./components/Layout";
